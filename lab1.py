3e6b18c8daa3e8f30cb2a7a86f4f03104cca684b
Vorobets <sashavorobets25@gmail.com>
2025-09-11 13:43:16 +0300

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,16 @@
+age = 18
+print('age', type(age),':', age)
+temperature=37.4
+print('temperature', type(temperature),':', temperature)
+name="Oleksandr"
+print("name", type(name),":", name)
+is_student=True
+print("is_student", type(is_student),":", is_student)
+vegetables=['огірок' , 'помідор' , 'броколі']
+print("vegetables", type(vegetables),":", vegetables)
+colors=['червоний', 'фіолетовий', 'синій']
+print("colors", type(colors),":", colors)
+birth_date= (2009 , 2 , 25)
+print("birth_date", type(birth_date),":", birth_date)
+film={"назва": "wearpons"}
+print("film", type(film),":", film)
